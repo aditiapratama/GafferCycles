08f2c05e2467eec1227299421102f72757e17f7e
Alex Fuller <boberfly@gmail.com>
2019-09-30 03:49:08 -0700

    Adding logging support

---
+++ dependencies/Gflags/config.py
@@ -0,0 +1,25 @@
+{
+
+	"downloads" : [
+
+		"https://github.com/gflags/gflags/archive/v2.2.2.tar.gz"
+
+	],
+
+	"license" : "COPYING.txt",
+
+	"commands" : [
+
+		"mkdir gafferBuild",
+		"cd gafferBuild &&"
+			" cmake"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_CXX_COMPILER={cxxCompiler}"
+			" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			" -D CMAKE_PREFIX_PATH={buildDir}"
+			" ..",
+		"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+	],
+
+}
---
+++ dependencies/Glog/config.py
@@ -0,0 +1,26 @@
+{
+
+	"downloads" : [
+
+		"https://github.com/google/glog/archive/v0.4.0.tar.gz"
+
+	],
+
+	"license" : "COPYING",
+
+	"commands" : [
+
+		"mkdir gafferBuild",
+		"cd gafferBuild &&"
+			" cmake"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_C_COMPILER={cCompiler}"
+			" -D CMAKE_CXX_COMPILER={cxxCompiler}"
+			" -D CMAKE_INSTALL_PREFIX={buildDir}"
+			" -D CMAKE_PREFIX_PATH={buildDir}"
+			" ..",
+		"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+	],
+
+}
